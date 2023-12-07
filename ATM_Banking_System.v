48624aa0a23e6e438ee059e6fcc00e500e53c9e4
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2023-12-08 01:24:14 +0200

    Transfer bas sttill incomplete

--- ATM_Banking_System.v
+++ ATM_Banking_System.v
@@ -2,30 +2,33 @@ module ATM (
 input[2:0] opcode,
 input[3:0] password,
 input allowwithdraw,take_receipt,
-input wire  [31:0] withdraw_amount,
-// don't forget to add your outputs 
+input wire  [15:0] Pers_Account_No,
+input wire  [31:0] withdraw_amount,Transfer_Amount,
+output reg Transfer_Done// don't forget to add your outputs 7ader
 );
 
-reg[1:0] chances = 2'00;
+reg[1:0] chances = 2'b00;`
 reg[3:0] visa_password = 4'b1110;
 reg[3:0] current_state, next_state;
 reg [31:0] existing_amount = 32'h000186A0;
+reg [15:0] Correct_Account_No = 16'hD903;
 
 parameter[3:0]  insert_card_state = 4'b0000,
-                language_state = 4'b0001,
-                pin_state = 4'b0010,
-                home_state = 4'b0011
-                balance_state = 4'b0100,
-                withdraw_state = 4'b0101,
-                deposit_state = 4'b0110,
-                transfer_state = 4'b0111,
-                display_state = 4'b1000,
+                language_state    = 4'b0001,
+                pin_state         = 4'b0010,
+                home_state        = 4'b0011,
+                balance_state     = 4'b0100,
+                withdraw_state    = 4'b0101,
+                deposit_state     = 4'b0110,
+                transfer_state    = 4'b0111,
+                display_state     = 4'b1000,
                 allow_withdraw_state = 4'b1001,
-                amount_state = 4'b1010,
+                amount_state      = 4'b1010,
                 allow_transfer_state = 4'b1011,
-                confirm_state = 4'b1100,
+                confirm_state      = 4'b1100,
                 print_state = 4'b1101,
-                eject_card_state = 4'b1110;
+                eject_card_state = 4'b1110,
+                Confirm_account_state=4'b1111;
 					
 					 
 
@@ -103,7 +106,23 @@ parameter[3:0]  insert_card_state = 4'b0000,
 
                                     next_state=home_state;
                                 end
-                            
+    transfer_state     		: begin
+									next_state = Confirm_account_state ;			  
+								end
+							
+    Confirm_account_state   : begin
+								if(Pers_Account_No != Correct_Account_No)
+									next_state = Confirm_account_state ;
+								else 
+									next_state = allow_transfer_state ;			  
+								end
+							
+    allow_transfer_state     	: begin
+								if(Transfer_Amount <= existing_amount)
+									next_state = Confirm_Transfer ;
+								else
+									next_state = allow_transfer_state ;			  
+								end
     endcase
     end
 
