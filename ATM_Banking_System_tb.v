dcf25bdb48cbee4dae84750fd1c21a63b4b74a09
adelmohamed11 <129177338+adelmohamed11@users.noreply.github.com>
2023-12-13 21:43:19 +0200

    Update ATM_Banking_System_tb.v

--- ATM_Banking_System_tb.v
+++ ATM_Banking_System_tb.v
@@ -8,8 +8,9 @@ module ATM_TB;
   reg [16:0] Pers_Account_No, ur_account;
   reg [18:0] withdraw_amount, Transfer_Amount, deposit_amount;
   wire Transfer_Successfully, ATM_Usage_Finished, Balance_Shown, Deposited_Successfully, Withdrew_Successfully,
-  Pin_Changed_Successfully, Receipt_Printed;
+    Pin_Changed_Successfully, Receipt_Printed;
 
+  // Instantiate the ATM module
   ATM uut (
     .clk(clk),
     .reset(reset),
@@ -38,11 +39,15 @@ module ATM_TB;
     .Receipt_Printed(Receipt_Printed)
   );
 
+  integer i;
+
+  // Clock generation
   initial begin
     clk = 0;
     forever #5 clk = ~clk;
   end
 
+  // Initialize inputs
   initial begin
     reset = 0;
     Card_in = 0;
@@ -62,24 +67,24 @@ module ATM_TB;
     Transfer_Amount = 18'h00000;
     deposit_amount = 18'h00000;
 
-    integer i
-
+    // Check reset 
     @(negedge clk)
-    if (
-      Transfer_Successfully != 0 || ATM_Usage_Finished != 0 || Balance_Shown != 0 ||
-      Deposited_Successfully != 0 || Withdrew_Successfully != 0 ||
-      Pin_Changed_Successfully != 0 || Receipt_Printed != 0
-    )
+    if (Transfer_Successfully || ATM_Usage_Finished || Balance_Shown || Deposited_Successfully || 
+        Withdrew_Successfully || Pin_Changed_Successfully || Receipt_Printed) begin
       $display("Error! Reset Issue");
+      $stop;    
+    end
 
     reset = 1'b1;
-    for ( i = 0; i <= 5000; i = i + 1) begin
+
+    for (i = 0; i <= 1000; i = i + 1) begin
+      @(negedge clk) 
       Card_in = $random();
       Language = $random();
       Timer = $random();
       money_counting = $random();
       another_transaction_bit = $random();
-      opcode = $random();
+      opcode = $urandom % 8;  // Randomize opcode in the range [0, 7]
       password = $random();
       new_pin = $random();
       allowwithdraw = $random();
@@ -91,24 +96,8 @@ module ATM_TB;
       Transfer_Amount = $random();
       deposit_amount = $random();
 
-/*
-      @(negedge clk)
-      if (
-        Card_in == 1'b0 && opcode == 3'b001 && take_receipt == 1'b1 &&
-        allowwithdraw == 1'b0 && another_transaction_bit == 1'b0 && Balance_Shown != 1'b0
-      )
-        $display("Error! Check Balance Issue while not inserting a card");
-
-      @(negedge clk)
-      if (
-        Card_in == 1'b1 && opcode == 3'b001 && take_receipt == 1'b1 &&
-        allowwithdraw == 1'b0 && another_transaction_bit == 1'b0 && Balance_Shown != 1'b1
-      )
-        $display("Error! Check Balance Issue while a card is inserted");
 
     end
-*/
-    $stop();
   end
-  end
+
 endmodule
